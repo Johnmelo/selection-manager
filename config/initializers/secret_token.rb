046b5b7e25d8f93711ec701da4a35da375075694
pontoka <kauepbd@gmail.com>
2013-07-06 10:18:06 -0300

    Criação

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SelectionManager::Application.config.secret_token = '2b40cf14d773d056c7362084bca0fb274a562125abdcb5ca13a434e0344ea8f2fcad2c3291c73412bbd9e8a9731ebc5699e9c52027109b8bbfbb52223efb1451'
